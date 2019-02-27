6cb486cc33eecf2ca33be1ffdb8b1bbb1b327f81
Justin Conklin <xtacide@gmail.com>
2019-02-27 00:31:32 -0500

    fix(iconLibrary): add icons to library

--- src/app/core/icon-library.ts
+++ src/app/core/icon-library.ts
@@ -1,7 +1,40 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheckCircle, faPalette } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBars,
+  faBell,
+  faChartBar,
+  faCheckCircle,
+  faChevronDown,
+  faCircle,
+  faClock,
+  faComments,
+  faEyeSlash,
+  faLifeRing,
+  faMoneyCheckAlt,
+  faPalette,
+  faReply,
+  faShoppingCart,
+  faSortDown,
+  faUserPlus,
+  faUsers,
+} from '@fortawesome/free-solid-svg-icons';
 
 export function installFontAwesomeIcons() {
+  library.add(faBell);
+  library.add(faChartBar);
+  library.add(faChevronDown);
+  library.add(faSortDown);
   library.add(faCheckCircle);
+  library.add(faComments);
+  library.add(faLifeRing);
   library.add(faPalette);
+  library.add(faShoppingCart);
+  library.add(faUsers);
+  library.add(faUserPlus);
+  library.add(faMoneyCheckAlt);
+  library.add(faClock);
+  library.add(faBars);
+  library.add(faReply);
+  library.add(faEyeSlash);
+  library.add(faCircle);
 }
