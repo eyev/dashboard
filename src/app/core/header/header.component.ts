c12bd681e18d83ad1d88fba90def477126f56cc1
Justin Conklin <xtacide@gmail.com>
2019-02-27 00:31:02 -0500

    fix(header): add user menu, notifications, sidenav toggle

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { slideInRight } from '@app/shared/animation-library';
 
 @Component({
   selector: 'eyev-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [slideInRight],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  isSidenavOpen = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  toggleMenu(state?: boolean) {
+    if (!state) {
+      this.isSidenavOpen = !this.isSidenavOpen;
+      return;
+    }
+    this.isSidenavOpen = state;
   }
-
 }
