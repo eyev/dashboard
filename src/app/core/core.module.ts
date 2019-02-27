b1195394e514127e2ffdbbd3ca6c035c1622103f
Justin Conklin <xtacide@gmail.com>
2019-02-27 00:31:16 -0500

    fix(coreModule): add new components to module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,12 +5,20 @@ import { installFontAwesomeIcons } from '@app/core/icon-library';
 import { SharedModule } from '@app/shared/shared.module';
 
 import { HeaderComponent } from './header/header.component';
+import { NotificationsIconComponent } from './header/notifications-icon/notifications-icon.component';
 import { ThemeSelectorComponent } from './header/theme-selector/theme-selector.component';
+import { UserMenuComponent } from './header/user-menu/user-menu.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 
 installFontAwesomeIcons();
 @NgModule({
-  declarations: [HeaderComponent, SidenavComponent, ThemeSelectorComponent],
+  declarations: [
+    HeaderComponent,
+    SidenavComponent,
+    ThemeSelectorComponent,
+    NotificationsIconComponent,
+    UserMenuComponent,
+  ],
   imports: [CommonModule, SharedModule],
   exports: [HeaderComponent, SidenavComponent],
 })
