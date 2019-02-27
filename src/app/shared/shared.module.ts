dd56c0bb1a1dfc4d29d82d20551041cab887166a
Justin Conklin <xtacide@gmail.com>
2019-02-27 00:35:02 -0500

    fix(sharedModule): add shared components to module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,29 @@ import { NgModule } from '@angular/core';
 import { windowProvider } from '@app/shared/utility/window.service';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+import { ChartCardComponent } from './chart-card/chart-card.component';
+import { GaugeChartComponent } from './charts/gauge-chart/gauge-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, FontAwesomeModule],
-  exports: [FontAwesomeModule],
+  declarations: [
+    ChartCardComponent,
+    LineChartComponent,
+    GaugeChartComponent,
+    LoadingSpinnerComponent,
+  ],
+  imports: [CommonModule, FontAwesomeModule, NgxChartsModule],
+  exports: [
+    FontAwesomeModule,
+    NgxChartsModule,
+    ChartCardComponent,
+    LineChartComponent,
+    GaugeChartComponent,
+    LoadingSpinnerComponent,
+  ],
   providers: [windowProvider],
 })
 export class SharedModule {}
