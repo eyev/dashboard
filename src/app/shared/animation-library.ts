8c6efcf9e4541e2a8a4a7d3fb46f896ae9b76039
Justin Conklin <justin@wiredviews.com>
2019-05-04 18:42:42 -0600

    fix(animationLibrary): update animations

--- src/app/shared/animation-library.ts
+++ src/app/shared/animation-library.ts
@@ -1,4 +1,10 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 export const slideInRight = trigger('slideInRight', [
   transition(':enter', [
@@ -8,4 +14,18 @@ export const slideInRight = trigger('slideInRight', [
   transition(':leave', [
     animate('220ms ease-in', style({ transform: 'translateX(-100%)' })),
   ]),
+]);
+export const slideInLeft = trigger('slideInLeft', [
+  transition(':enter', [
+    style({ transform: 'translateX(200%)' }),
+    animate('300ms ease-in', style({ transform: 'translateX(0%)' })),
+  ]),
+  transition(':leave', [
+    animate('300ms ease-in', style({ transform: 'translateX(200%)' })),
+  ]),
+]);
+export const fadeIn = trigger('fadeIn', [
+  state('in', style({ opacity: 1 })),
+  transition(':enter', [style({ opacity: 0 }), animate(300)]),
+  transition(':leave', animate(300, style({ opacity: 0 }))),
 ]);
